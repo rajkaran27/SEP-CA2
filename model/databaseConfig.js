8c3c39938eca2e54f39f4d6cdedb0e31184c7bcc
chenlexuann <LEXUAN.22@ichat.sp.edu.sg>
2023-07-05 19:24:53 +0800

    modified view/B/SG/furnitureCategory.html view/B/SG/retailProductsCategory.html

--- model/databaseConfig.js
+++ model/databaseConfig.js
@@ -4,7 +4,7 @@ var dbconnect = {
         var conn = mysql.createConnection({
             host: "localhost",
             user: "root",
-            password: "root1234",
+            password: "root",
             database: "islandfurniture-it07"
         });
         return conn;
